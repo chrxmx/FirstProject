4672357f9ca823e3dd45ca9d696741a6ea8ec969
Koncz <koncz.alexander@blathy.info>
2026-09-07 14:35:05 +0200

    bum

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,17 +1,24 @@
-﻿namespace FirstProject
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace FirstProject
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8604 // Possible null reference argument.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
             Console.Title = "név kérés";
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("ki vagy: ");
 
             string Name = Console.ReadLine();
             Console.WriteLine($"te vagy {Name}!");
+          
+           
+
+            }
         }
     }
-}
---
+++ calc/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("szám1: ");
+                double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("szám2: ");
+                double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("adj meg egy operátort");
+                string @operator = Console.ReadLine();
+
+                decimal result = 0;
+                switch (@operator)
+                {
+                    case "+":
+                        result = (decimal)(num1 + num2);
+                        break;
+                    case "-":
+                        result = (decimal)(num1 - num2);
+                        break;
+                    case "*":
+                        result = (decimal)(num1 * num2);
+                        break;
+                    case "/":
+                        {
+                            if (num2 == 0)
+                            {
+                                Console.WriteLine("nincs nullával osztás");
+                                continue;
+                            }
+                            result = (decimal)(num1 / num2);
+                        } break;
+                    default:
+                        Console.WriteLine("nem jó operátor!");
+                        continue;
+                }
+                Console.WriteLine(result);
+
+            }
+        }
+    }
+}
