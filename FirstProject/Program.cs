6e8d161783f7c5af622c7319987c65b847fe18e2
Koncz <koncz.alexander@blathy.info>
2026-09-07 12:19:52 +0200

    névbekérés

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace FirstProject
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title  = "név kérés";
+            Console.WindowWidth = 50; 
+            
+            Console.Write("ki vagy: ");
+
+            string Name = Console.ReadLine();
+            Console.WriteLine($"te vagy {Name}!");
         }
     }
 }
