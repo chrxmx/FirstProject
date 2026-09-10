f428c4ad5a0bd70c3b7d6a9524254e7bfeaf7e88
Rendszergazda <rendszergazda@blathy.info>
2026-09-10 12:01:04 +0200

    bobmboclat

--- calc/Program.cs
+++ calc/Program.cs
@@ -1,48 +1,72 @@
-﻿using System.Globalization;
-
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 namespace calc
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Calc";
+            Console.ForegroundColor = ConsoleColor.Green;
             while (true)
-            {
-                Console.Write("szám1: ");
-                double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("szám2: ");
-                double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.WriteLine("adj meg egy operátort");
-                string @operator = Console.ReadLine();
-
-                decimal result = 0;
-                switch (@operator)
+                try
                 {
-                    case "+":
-                        result = (decimal)(num1 + num2);
-                        break;
-                    case "-":
-                        result = (decimal)(num1 - num2);
-                        break;
-                    case "*":
-                        result = (decimal)(num1 * num2);
-                        break;
-                    case "/":
-                        {
-                            if (num2 == 0)
-                            {
-                                Console.WriteLine("nincs nullával osztás");
-                                continue;
-                            }
-                            result = (decimal)(num1 / num2);
-                        } break;
-                    default:
-                        Console.WriteLine("nem jó operátor!");
-                        continue;
-                }
-                Console.WriteLine(result);
+                    Console.Write("szám1: ");
+                    decimal num1 = decimal.Parse(Console.ReadLine()!);
+                    Console.Write("szám2: ");
+                    decimal num2 = decimal.Parse(Console.ReadLine()!);
+                    Console.WriteLine("adj meg egy operátort: +,-,*,^,/");
 
-            }
+                    
+                    string @operator = Console.ReadLine();
+
+                    decimal result = 0;
+                    switch (@operator)
+
+                    {
+
+                        case "+":
+                            result = (num1 + num2);
+                            break;
+                        case "-":
+                            result = (num1 - num2);
+                            break;
+                        case "*":
+                            result = (num1 * num2);
+                            break;
+                        case "^":
+                            result = (decimal)Math.Pow((double)num1, (double)num2);
+                            break;
+                        case "/":
+                            {
+                                if (num2 == 0)
+                                {   
+                                    Console.WriteLine("nincs nullával osztás");
+                                    continue;
+                                }
+                                result = (num1 / num2);
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("nem jó operátor!");
+                            continue;
+                    }
+                    Console.WriteLine(result);
+
+                    Console.WriteLine("írd azt hogy EXIT ha kiszeretnél lépni");
+                    string exit = Console.ReadLine();
+                    if (exit.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("nem számot adtál meg");
+                }
+
         }
     }
 }
