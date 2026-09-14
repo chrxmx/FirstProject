66ae261d2c7f4c362249faeac4ee9b2072f4080d
Koncz Alexander <koncz.alexander@blathy.info>
2026-09-14 17:43:09 +0200

    otthoni work

---
+++ AlapTetelek/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static int Osszegzes(int[] tomb)
+    {
+        int osszeg = 0;
+
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            osszeg += tomb[i];
+        }
+
+        return osszeg;
+    }
+
+    static void ParosSzamok(int[] tomb)
+    {
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            if (tomb[i] % 2 == 0)
+            {
+                Console.WriteLine(tomb[i]);
+            }
+        }
+    }
+    static void Abszolutszamok(int[] tomb)
+    {
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            Console.WriteLine(Math.Abs(tomb[i]));
+        }
+    }
+    static void Main()
+    {
+        int[] tomb = { -5, 10, 15, -20, 25 };
+
+        Console.WriteLine(Osszegzes(tomb));
+        ParosSzamok(tomb);
+        Abszolutszamok(tomb);
+    }
+}
