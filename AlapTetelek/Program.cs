92f712236ddd15998cfc73f96ffbf1b0e1feb870
Koncz Alexander <koncz.alexander@blathy.info>
2026-09-14 17:58:34 +0200

    gyak

--- AlapTetelek/Program.cs
+++ AlapTetelek/Program.cs
@@ -31,12 +31,51 @@ class Program
             Console.WriteLine(Math.Abs(tomb[i]));
         }
     }
+    static void NegativSzamok(int[] tomb)
+    {
+        int paratlanok = 0;
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            if (tomb[i] < 0)
+            {
+                paratlanok++;
+            }
+        }
+        Console.WriteLine(paratlanok);
+    }
+    static void LegNagyobbSzam(int[] tomb)
+    {
+        int max = tomb[0];
+        for (int i = 1; i < tomb.Length; i++)
+        {
+            if (tomb[i] > max)
+            {
+                max = tomb[i];
+            }
+        }
+        Console.WriteLine(max);
+    }
+    static void HanyParosSzam(int[] tomb)
+    {
+        int parosok = 0;
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            if (tomb[i] % 2 == 0)
+            {
+                parosok++;
+            }
+        }
+        Console.WriteLine(parosok);
+    }
     static void Main()
     {
         int[] tomb = { -5, 10, 15, -20, 25 };
 
-        Console.WriteLine(Osszegzes(tomb));
-        ParosSzamok(tomb);
-        Abszolutszamok(tomb);
+        ///Console.WriteLine(Osszegzes(tomb));
+        ///ParosSzamok(tomb);
+        ///Abszolutszamok(tomb);
+        ///NegativSzamok(tomb);
+        ///LegNagyobbSzam(tomb);
+        ///HanyParosSzam(tomb);
     }
 }
